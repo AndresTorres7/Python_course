e186530c24ead98e05607b643f22ce90098eb6d2
AndresTorres7 <98290511+AndresTorres7@users.noreply.github.com>
2026-05-27 01:53:54 -0500

    Delete clase4.py

--- clase4.py
+++
@@ -1,8 +0,0 @@
-name = 'Andres Felipe'
-last_name = "    Ramos Torres"
-print(5*name)
-print(name + " " + last_name)
-print(len(name))
-print(name.lower())
-print(name.upper())
-print(last_name.strip())
